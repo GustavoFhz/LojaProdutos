d60a6c668e5fc1f441ff6a425f06738868f35a8e
Gustavo <gustavojesus79@gmail.com>
2026-01-22 17:16:14 -0300

    Criação Dto Cadastrar Produto

---
+++ LojaProdutos/Dto/Produto/CriarProdutoDto.cs
@@ -0,0 +1,24 @@
+﻿using LojaProdutos.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaProdutos.Dto.Produto
+{
+    public class CriarProdutoDto
+    {
+        [Required(ErrorMessage = "Digite o nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite a marca!")]
+        public string Marca { get; set; }
+        [Required(ErrorMessage = "Insira a foto!")]
+        public string Foto { get; set; }
+        [Required(ErrorMessage = "Insira o valor!")]
+        public double Valor { get; set; }
+        [Required(ErrorMessage = "Insira a quantidade em estoque!")]
+        public int QuantidadeEstoque { get; set; }
+        [Required(ErrorMessage = "Selecione a categoria!")]
+
+        public int CategoriaModelId { get; set; }
+
+    }
+}
