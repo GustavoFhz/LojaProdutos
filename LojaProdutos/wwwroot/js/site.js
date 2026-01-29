2a86a230c40adf5b2870e6f0a7fc2a748d9f7215
Gustavo <gustavojesus79@gmail.com>
2026-01-28 22:23:13 -0300

    Filtro Tabelas

--- LojaProdutos/wwwroot/js/site.js
+++ LojaProdutos/wwwroot/js/site.js
@@ -1,6 +1,69 @@
 ﻿// Aguarda o carregamento completo do DOM antes de executar o código
 $(document).ready(function () {
 
+    //Configuração JS - Filtros:
+
+    $('#registros').DataTable({
+
+        "ordering": true,
+
+        "paging": true,
+
+        "searching": true,
+
+        "oLanguage": {
+
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+
+            "sInfoPostFix": "",
+
+            "sInfoThousands": ".",
+
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+
+            "sLoadingRecords": "Carregando...",
+
+            "sProcessing": "Processando...",
+
+            "sZeroRecords": "Nenhum registro encontrado",
+
+            "sSearch": "Pesquisar",
+
+            "oPaginate": {
+
+                "sNext": "Proximo",
+
+                "sPrevious": "Anterior",
+
+                "sFirst": "Primeiro",
+
+                "sLast": "Ultimo"
+
+            },
+
+            "oAria": {
+
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+
+            }
+
+        }
+
+    });
+
+
+});
+
+
+
     // Define um atraso de 4000 milissegundos (4 segundos)
     setTimeout(function () {
 
@@ -15,4 +78,4 @@ $(document).ready(function () {
 
     }, 4000);
 
-});
+   
